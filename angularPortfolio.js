38c2a51893c8226f0bb581eeac0655223c7d6b27
Emily Sanchez <emily.sanchez@contractor.farmersinsurance.com>
2019-05-20 08:47:40 -0700

    addking pages to route to

---
+++ angularPortfolio.js
@@ -0,0 +1,47 @@
+var portfolioApp = angular.module('myPortfolio', []);
+
+// this is what will route to other pages that are specified below
+// portfolioApp.config(function($routeProvider) {
+//     $routeProvider
+
+//         // route for the home page
+//         .when('/', {
+//             templateUrl : 'home.html',
+//             controller  : 'mainController'
+//         })
+
+//         // route for the about page
+//         .when('/about', {
+//             templateUrl : 'about.html',
+//             controller  : 'aboutController'
+//         })
+
+//         // route for the contact page
+//         .when('/contact', {
+//             templateUrl : 'contact.html',
+//             controller  : 'contactController'
+//         })
+
+//          .when('/projects', {
+//              tempUrl : 'projects.html',
+//              controller : 'projectsController'
+//          });
+//     });
+     // create the controller and inject Angular's $scope
+     portfolioApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+    portfolioApp.controller('aboutController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+
+    portfolioApp.controller('contactController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+    portfolioApp.controller('projectsController', function($scope) {
+        $scope.message = 'Project page. This is the project page.';
+    });
+
